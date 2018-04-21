1b2ad0b08ec835141c21873d0789eb020d260233
Scotty <scotty@Scottys-MacBook-Pro.local>
2018-04-21 17:21:52 -0400

    scaffolded

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,15 @@
 <template>
   <div id="app">
-  
-  <header></header>
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+  <Header></Header>
+  <Content></Content>
+  <Footer></Footer>
   </div>
 </template>
 
 <script>
-
-
-import Header from "./sections/Header";
+import Header from "./sections/Header"
+import Content from "./sections/Content"
+import Footer from "./sections/Footer"
 
 export default {
   name: 'app',
--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
 
 new Vue({
-  el: '#app',
-  render: h => h(App)
-})
+  el: "#app",
+  render: h => h(App),
+  template: "</app>",
+  components: {
+    App
+  }
+});
