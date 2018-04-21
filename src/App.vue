45096460cc7d2e4bf6c48021daa11e91b093402f
Jesus <jesus_artteaga@hotmail.com>
2018-04-21 18:51:12 -0400

    test

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,47 @@
 <template>
-  <div id="app" class="container">
-  <div class="row">
-  <div class="col">
-    <Header></Header>
+  <div id="app">
+    <div class="jumbotron">
+      <div class="container">
+        <h1 class="display-3">{{ msg }}</h1>
+        <p>Enter the number, avoid Awkward Conversations Now!</p>
+        <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more &raquo;</a></p>
+      </div>
     </div>
-  </div>
-   <div class="row justify-content-md-center">
-     <Content></Content>
-  </div>
-   <div class="row">
-      <div class="col">
+    
+<div class="container">
+      <!-- Example row of columns -->
+      <div class="row">
+        <div class="col-md-4">
+          <h2>What</h2>
+          <p>Voidder is SPA that helps you avoid conversations . </p>
+          <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+        </div>
+        <div class="col-md-4">
+          <h2>How</h2>
+          <p>We allow our software to contact the person you want to lose contact with. </p>
+          <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+       </div>
+        <div class="col-md-4">
+          <h2>Why</h2>
+          <p>We Felt it was neccesary .</p>
+          <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+        </div>
+      </div>
 
-     <Footer></Footer>
-     </div>
-  </div>
+      <hr>
+
+      <footer>
+        <p>&copy; Company 2017</p>
+      </footer>
+    </div> <!-- /container -->
+    
+    <h2>Ecosystem</h2>
+    <ul>
+      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
+      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
+      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
+      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
+    </ul>
   </div>
 </template>
 
@@ -25,7 +53,7 @@ export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Voidder'
     }
   },
   components: {
