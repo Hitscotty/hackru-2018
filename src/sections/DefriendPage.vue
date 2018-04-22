ad155c057582b2325e5a5cedd8a59603d4226d16
Scotty <scotty@Scottys-MacBook-Pro.local>
2018-04-21 20:53:43 -0400

    working on defriender page

--- src/sections/DefriendPage.vue
+++ src/sections/DefriendPage.vue
@@ -1,8 +1,47 @@
 <template>
-    <h1> DEFRIENDER </h1>
+  <div class="container">
+    <div class="heading text-center py-4">
+      <p class="h2">
+        Our experts love to answer questions.
+        <br> Fill out the form below.
+      </p>
+    </div>
+    <div class="row justify-content-md-center">
+      <div class="col-6">
+        <div class="card">
+          <div class="uk-grid">
+            <div class="text-center">
+              <form class="ui form">
+                <div class="field inline">
+                  <label>
+                    Phone #
+                  </label>
+                  <input name="phone" placeholder="(xxx) xxx xxxx" />
+                </div>
+                <div class="field inline">
+                  <label for="embedpollfileinput" class="ui huge green floated button" @click="phoneM">
+                    <i class="ui upload icon"></i>
+                    Upload Audio
+                  </label>
+                </div>
+                <button class="ui green button"> send </button>
+
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
-    export default{
+  export default {
+    methods: {
+      phoneM() {
+        swal("Wait", "Why not just call them then?");
 
+      }
     }
+  }
+
 </script>
